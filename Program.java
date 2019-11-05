3c33869008e42a4af4aaf044dc79127593d2c192
Krzysztof <krajzys@o2.pl>
2019-11-05 14:09:18 +0100

    Dodaje komentarze do kodu

--- Program.java
+++ Program.java
@@ -1,12 +1,21 @@
 /* This is a simple Java program. 
    FileName : "HelloWorld.java". */
+
+// To jest klasa
 class HelloWorld 
 { 
     // Your program begins with a call to main(). 
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
+	// To jest komentarz
         System.out.println("Hello, World"); 
+	// To jest drugi komentarz
 	System.out.println("Hello, World"); 
+	// To już trzeci komentarz!
+	
+
+	// Kolejny komentarz
+	// Hej
     } 
 } 
